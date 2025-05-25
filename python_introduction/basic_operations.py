0d4118d26068bb7d45b86f700e87c681e43606dc
Michael Omosebi <omosebimichael@live.com>
2025-05-25 01:02:30 +0100

    add my repo directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,22 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+division_result = number1 / number2
+modulus_result = number1 % number2
+
+def display_results():
+    print(f"Addition of 10 and 5 is {sum_result}")
+    print(f"Subtraction of 10 and 5 is {subtraction_result}")
+    print(f"Multiplication of 10 and 5 is {multiplication_result}")
+    # print(f"Division: {division_result}")
+    # print(f"Modulus: {modulus_result}")
+
+if __name__ == "__main__":
+    display_results()
+    # Uncomment the following lines to see division and modulus results
+    # print(f"Division of 10 by 5 is {division_result}")
+    # print(f"Modulus of 10 by 5 is {modulus_result}")
+
