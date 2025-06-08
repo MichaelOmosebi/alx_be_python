55111f937d8c66c93a68b36ce1b2ad7f2c19956f
Michael Omosebi <omosebimichael@live.com>
2025-06-08 18:26:55 +0100

    modify temperature converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -27,7 +27,7 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    # global FAHRENHEIT_TO_CELSIUS_FACTOR
     try:
         celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
         return celsius
@@ -36,7 +36,7 @@ def convert_to_celsius(fahrenheit):
     
 def convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit."""
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    # global CELSIUS_TO_FAHRENHEIT_FACTOR
     try:
         fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
         return fahrenheit
