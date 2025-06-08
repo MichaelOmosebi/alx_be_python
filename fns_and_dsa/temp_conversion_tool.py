3df7d1c4891bd0551143b451081d313e1ca71401
Michael Omosebi <omosebimichael@live.com>
2025-06-08 18:23:31 +0100

    modify temperature converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -22,6 +22,7 @@
 # Use input validation to ensure that the user enters a valid temperature and unit.
 
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
